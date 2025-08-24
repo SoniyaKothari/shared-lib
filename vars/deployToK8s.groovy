3f6d5bad572731a041d58e16860823f3ce121f1b
Soniya Kothari <soniyakothari1511@gmail.com>
2025-08-24 19:28:59 +0530

    Update deployToK8s.groovy

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -3,6 +3,7 @@ def call()
     withEnv(["KUBECONFIG=/var/lib/jenkins/.kube/config"]) 
     {
         sh """
+          kubectl apply -f k8s/namespace.yml  
           kubectl apply -f k8s/Deployment.yml
           kubectl apply -f k8s/Service.yml
           kubectl rollout status deployment/weather -n devops
