aa7ddf6a0d4fe3a7da25abbceb2610fd8578bfd4
Soniya Kothari <soniyakothari1511@gmail.com>
2025-08-24 17:55:07 +0530

    Update deployToK8s.groovy

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -6,7 +6,6 @@ def call()
           kubectl apply -f k8s/Deployment.yml
           kubectl apply -f k8s/Service.yml
           kubectl rollout status deployment/weather -n devops
-          nohup /var/lib/jenkins/start-port-forward.sh > /var/lib/jenkins/port-forward.log 2>&1 &
         """
     }
 }
